24d8f81f9b8532ced734c4d1f8bbbbfef84b09db
GanstaKingofSA <azarieldc@gmail.com>
2022-08-29 13:53:02 -0500

    more improvements

--- launcher/game/extractor.py
+++ launcher/game/extractor.py
@@ -4,20 +4,21 @@ import os
 import shutil
 import sys
 
+
 class Extractor:
-    '''
+    """
     This class is responsible for the extraction of
     zip files for DDLC mods.
-    '''
+    """
 
     def __init__(self):
-        self.renpy_script_contents = ('.rpa', '.rpyc', '.rpy')
-        
+        self.renpy_script_contents = (".rpa", ".rpyc", ".rpy")
+
     def valid_zip(self, filePath):
-        '''
+        """
         This define checks if the ZIP is a valid DDLC mod.
         archive.
-        '''
+        """
 
         contents = []
 
@@ -28,22 +29,12 @@ class Extractor:
             if x.endswith((self.renpy_script_contents)):
                 contents = []
                 return True
-                
+
         return False
 
     def game_installation(self, filePath, modFolder, copy=False):
-        '''
-        This define extracts the game/tool archive to the temp folder.
-
-        filePath - The given game zip package.\n
-        modFolder - The mod folder inside the mod install folder.\n
-        copy - Makes sure this is a copy or a ZIP we are working with.
-        '''
-
-        os.makedirs(modFolder)
-
         if not copy:
-            td = tempfile.mkdtemp(prefix="NewDDML_",suffix="_TempGame")
+            td = tempfile.mkdtemp(prefix="NewDDML_", suffix="_TempGame")
 
             with ZipFile(filePath, "r") as z:
                 z.extractall(td)
@@ -55,64 +46,23 @@ class Extractor:
         else:
             game_dir = filePath
 
-        for temp_src, dirs, files in os.walk(game_dir):
-            dst_dir = temp_src.replace(game_dir, modFolder)
-            
-            for d in dirs:
-                try: os.makedirs(os.path.join(dst_dir, d))
-                except OSError: continue
-            
+        for temp_src, dirs, files in os.walk(game_dir + "/game"):
             for f in files:
-                shutil.move(os.path.join(temp_src, f), os.path.join(dst_dir, f))
-        
+                base, ext = os.path.splitext(f)
+
+                if not (ext in self.renpy_script_contents):
+                    continue
+
+                if base != "scripts":
+                    src_dir = os.path.join(temp_src, f)
+                    dst_dir = src_dir.replace(temp_src, os.path.join(modFolder, "game"))
+                    shutil.move(src_dir, dst_dir)
+
         if not copy:
             shutil.rmtree(game_dir)
 
-        if sys.platform == "darwin":
-            try: os.remove(os.path.join(modFolder, "DDLC.app/Contents/Resources/autorun/game/scripts.rpa"))
-            except: pass
-        else:
-            try: os.remove(os.path.join(modFolder, "game/scripts.rpa"))
-            except: pass
-            
-    def installation(self, filePath, modFolder, copy=False):
-        '''
-        This define extracts the mod archive to the temp folder and installs it
-        to the mod folder.
-        
-        filepath - The given mod zip package.\n
-        modFolder - The mod folder inside the mod install folder.
-        copy - Makes sure this is a copy or a ZIP we are working with.
-        '''
+    def installation(self, filePath, modFolder):
+        os.makedirs(modFolder)
 
-        if not copy:
-
-            with ZipFile(filePath, "r") as z:
-                if sys.platform == "darwin":
-                    z.extractall(os.path.join(modFolder, "DDLC.app/Contents/Resources/autorun"))
-                else:
-                    z.extractall(modFolder)
-
-        else:
-            mod_dir = filePath
-
-            modFolder = os.path.join(modFolder, "DDLC.app/Contents/Resources/autorun")
-
-            for mod_src, dirs, files in os.walk(mod_dir):
-                dst_dir = mod_src.replace(mod_dir, modFolder)
-                
-                for d in dirs:
-                    try: os.makedirs(os.path.join(dst_dir, d))
-                    except OSError: continue
-                
-                for f in files:
-                    mod_file = os.path.join(mod_src, f)
-                    dst_file = os.path.join(dst_dir, f)
-
-                    if os.path.exists(dst_file):
-                        if os.path.samefile(mod_file, dst_file):
-                            continue
-
-                        os.remove(dst_file)
-
-                    shutil.move(mod_file, dst_file)
+        with ZipFile(filePath, "r") as z:
+            z.extractall(modFolder)
--- launcher/game/tool_install.rpy
+++ launcher/game/tool_install.rpy
@@ -51,41 +51,49 @@ label tool_install:
                     td = tempfile.mkdtemp(prefix="DDMM_",suffix="_TempTool")
                     with ZipFile(path, "r") as z:
                         z.extractall(td)
-                        
-                if glob.glob(td + "/game"):
+
+                tool_dir = None 
+                for src, dirs, files in os.walk(td):
+                    for d in dirs:
+                        for x in ["game", "mod_assets", "python-packages"]:
+                            if x in d:
+                                if "game" in d:
+                                    tool_dir = os.path.join(src, d)
+                                    gamedir = True
+                                else:
+                                    tool_dir = os.path.join(src,d).replace("\\" + d, "")
+                                    gamedir = True
+                                break
+                if tool_dir is None:
+                    # Assume the best is the src itself
                     tool_dir = td
-                elif glob.glob(td + "/*/game"):
-                    tool_dir = td + "/" + glob.glob(td + "/*/game")[0].replace("\\", "/").split("/")[-2]
-                else:
-                    tool_dir = td
-                    gamedir = True
                     
-        with interface.error_handling("extracting user tool"):      
-            for tool_src, dirs, files in os.walk(tool_dir):
-                if gamedir:
-                    dst_dir = tool_src.replace(tool_dir, project_dir + "/game")
-                else:
-                    dst_dir = tool_src.replace(tool_dir, project_dir)
+            with interface.error_handling("extracting user tool pt2"):      
+                for tool_src, dirs, files in os.walk(tool_dir):
+                    if gamedir:
+                        dst_dir = tool_src.replace(tool_dir, project_dir + "/game")
+                    else:
+                        dst_dir = tool_src.replace(tool_dir, project_dir  + "/game/mod_assets")
+                    
+                    for d in dirs:
+                        if not os.path.exists(os.path.join(dst_dir, d)):
+                            os.makedirs(os.path.join(dst_dir, d))
                 
-                for d in dirs:
-                    if not os.path.exists(os.path.join(dst_dir, d)):
-                        os.makedirs(os.path.join(dst_dir, d))
-            
-                for f in files:
-                    temp_file = os.path.join(tool_src, f)
-                    dst_file = os.path.join(dst_dir, f)
-                    
-                    if os.path.exists(dst_file):
-                        if renpy.windows:
-                            if os.stat(temp_file) == os.stat(dst_file):
-                                continue
-                        else:
-                            if os.path.samefile(temp_file, dst_file):
-                                continue
+                    for f in files:
+                        temp_file = os.path.join(tool_src, f)
+                        dst_file = os.path.join(dst_dir, f)
+                        
+                        if os.path.exists(dst_file):
+                            if renpy.windows:
+                                if os.stat(temp_file) == os.stat(dst_file):
+                                    continue
+                            else:
+                                if os.path.samefile(temp_file, dst_file):
+                                    continue
 
-                        os.remove(dst_file)
+                            os.remove(dst_file)
 
-                    shutil.move(temp_file, dst_file)
+                        shutil.move(temp_file, dst_file)
 
             if not renpy.macintosh and not persistent.safari:
                 shutil.rmtree(td)
